4b31bde62faf238c5c28d88dd2edbcce1045dae8
pracdirs <86368935+pracdirs@users.noreply.github.com>
2021-12-03 09:58:37 -0500

    x

---
+++ autocommit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd C:/Work/CheckListEficacia
+git add --all
+timestamp() {
+  date +"at %H:%M:%S on %d/%m/%Y"
+}
+git commit -am "Regular auto-commit $(timestamp)"
+git push origin develop:develop-backup
